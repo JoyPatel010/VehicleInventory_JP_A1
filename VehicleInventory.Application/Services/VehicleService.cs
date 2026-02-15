62767493a05eb02ebd0112549d2604a24225f31c
Joy Patel <pateljoy149@gmail.com>
2026-02-14 22:58:32 -0500

    Created VehicleService application service

---
+++ VehicleInventory.Application/Services/VehicleService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleInventory.Application.DTOs;
+using VehicleInventory.Application.Interfaces;
+using VehicleInventory.Domain.Entities;
+
+namespace VehicleInventory.Application.Services
+{
+    public class VehicleService
+    {
+        private readonly IVehicleRepository _repository;
+
+        public VehicleService(IVehicleRepository repository)
+        {
+            _repository = repository;
+        }
+    }
+}
