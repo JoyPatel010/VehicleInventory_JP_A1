9e28254fc96bd24006d47c7eafe4f52f5dc5252d
Joy Patel <pateljoy149@gmail.com>
2026-02-14 20:36:42 -0500

    For persistence, define the IVehicleRepository abstraction

---
+++ VehicleInventory.Application/Interfaces/IVehicleRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleInventory.Domain.Entities;
+
+namespace VehicleInventory.Application.Interfaces
+{
+    public interface IVehicleRepository
+    {
+        Task AddAsync(Vehicle vehicle);
+        Task<Vehicle> GetByIdAsync(Guid id);
+        Task<List<Vehicle>> GetAllAsync();
+        Task UpdateAsync(Vehicle vehicle);
+        Task DeleteAsync(Vehicle vehicle);
+    }
+}
