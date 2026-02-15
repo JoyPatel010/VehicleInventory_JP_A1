2e8b2ba377249c2652040fa610e54e4ca5a2f0ec
Joy Patel <pateljoy149@gmail.com>
2026-02-14 19:19:48 -0500

    To return vehicle data, add VehicleDto

---
+++ VehicleInventory.Application/DTOs/VehicleDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleInventory.Domain.Enums;
+
+namespace VehicleInventory.Application.DTOs
+{
+    public class VehicleDto
+    {
+        public Guid Id { get; set; }
+        public string VehicleCode { get; set; }
+        public string LocationId { get; set; }
+        public string VehicleType { get; set; }
+        public VehicleStatus Status { get; set; }
+    }
+}
