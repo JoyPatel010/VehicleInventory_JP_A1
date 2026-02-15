8fc7ec5665eceb82b563bb885bb4856ec526e3b9
Joy Patel <pateljoy149@gmail.com>
2026-02-14 20:27:09 -0500

    For status updates, include UpdateVehicleStatusDto

---
+++ VehicleInventory.Application/DTOs/UpdateVehicleStatusDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using VehicleInventory.Domain.Enums;
+
+namespace VehicleInventory.Application.DTOs
+{
+    public class UpdateVehicleStatusDto
+    {
+        [Required]
+        public VehicleStatus Status { get; set; }
+    }
+}
